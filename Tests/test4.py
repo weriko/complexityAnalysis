0c4240a66e55bcde974638f1f8fb43e3edaf0424
weriko <53843483+weriko@users.noreply.github.com>
2020-09-24 11:25:25 -0500

    Update test4.py

--- Tests/test4.py
+++ Tests/test4.py
@@ -1,1 +1,14 @@
-s
+x = 5
+for p in range(x):
+  for i in range(x):
+    for j in range(x):
+      for k in range(x):
+        for m in range(x):
+          print(5)
+  for b in range(x):
+    for asd in range(x):
+      print(5)
+    for dfgkd in range(x):
+      for kas in range(x):
+        for u in range(x):
+          print(x)
