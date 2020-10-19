e84df4b63faf7c8b9133a1a5427e410516ebea80
weriko <53843483+weriko@users.noreply.github.com>
2020-10-19 18:44:28 -0500

    Add files via upload

---
+++ Tests/test5.py
@@ -0,0 +1,13 @@
+def f(x):
+    return x**2
+def ducu(x):
+    print("hello ducu")
+    print("hello can")
+def vector(x):
+    return "hello vector"
+def foo(x):
+    print(5+3)
+    
+    print(7**2)
+    print("ERRORRRRR")
+    return x**7**2
