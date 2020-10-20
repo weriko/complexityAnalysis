8121f64bac1aac246c3a99082f8d019d575e6ef0
weriko <53843483+weriko@users.noreply.github.com>
2020-10-20 00:48:03 -0500

    Update test5.py

--- Tests/test5.py
+++ Tests/test5.py
@@ -24,3 +24,17 @@ def fibonacci5(x):
         
         return x
     return fibonacci5(x-1)+ fibonacci5(x-2)
+  
+def bubble(x):
+    
+    l = [i for i in range(x,0,-1)]
+    for p in range(len(l)-1):
+        for i in range(len(l)-1):
+            for j in range(len(l)-1):
+                if l[j]>l[j+1]:
+                    temp = l[j]
+                    l[j]=l[j+1]
+                    l[j+1]=temp
+    return l
+
+
