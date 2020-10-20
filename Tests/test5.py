7afc388913a8c5cd3f2ab2ffff99826e801c0499
weriko <53843483+weriko@users.noreply.github.com>
2020-10-19 19:23:08 -0500

    Update test5.py

--- Tests/test5.py
+++ Tests/test5.py
@@ -1,11 +1,15 @@
 def f(x):
+    
     return x**2
 def ducu(x):
+    
     print("hello ducu")
     print("hello can")
 def vector(x):
+    
     return "hello vector"
 def foo(x):
+    
     print(5+3)
     
     print(7**2)
